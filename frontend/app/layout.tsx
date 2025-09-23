7837f93cf80b035aa554794774d4e49fc30ca415
Win Lin <hey@winl.in>
2025-09-23 10:15:13 -0400

    Add Google Tag Manager (GTM-KCLJ76N4) to all pages
    - Added GTM script to head section using beforeInteractive strategy
    - Added GTM noscript iframe immediately after body tag opens
    - Enables advanced analytics and tracking across all pages

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -170,6 +170,16 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProviderWrapper>
+      {/* Google Tag Manager - beforeInteractive loads in head */}
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KCLJ76N4');
+        `}
+      </Script>
       {/* Google tag (gtag.js) - beforeInteractive loads in head */}
       <Script
         async
@@ -188,6 +198,15 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KCLJ76N4"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <PostHogProviderWrapper>
             <Suspense fallback={null}>
               <PostHogPageView />
